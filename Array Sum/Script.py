4a99f97921561fd3079a70bc3aa2b45a52a7766a
abbasmalik514 <nabbas.jani2@gmail.com>
2020-04-05 11:20:51 +0500

    Create Script.py

---
+++ Array Sum/Script.py
@@ -0,0 +1,20 @@
+import os
+
+def simpleArraySum(ar):
+    array_sum=0
+    for element in ar:
+        array_sum += element
+    return array_sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
